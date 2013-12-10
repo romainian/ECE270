f3d5b635129bb49ac153c811a6e6498771b601d6
houghtonbrad <houghtonbradley@yahoo.com>
2013-12-09 19:55:17 -0500

    Update drawRectangle.c

--- FinalProject/BasicFunctions/drawRectangle.c
+++ FinalProject/BasicFunctions/drawRectangle.c
@@ -1,4 +1,6 @@
-void drawRectangle(float x, float y, float width, float height, int fill_r, int fill_g, int fill_b, float fill_opacity, int stroke_r, int stroke_g, int stroke_b, float stroke_opacity, int stroke_width)
+void drawRectangle(float x, float y, float width, float height, 
+                    int fill_r, int fill_g, int fill_b, float fill_opacity, 
+                    int stroke_r, int stroke_g, int stroke_b, float stroke_opacity, int stroke_width)
     {
         fprintf(fp,"\n <rect x = '%f' y = '%f' width = '%f' height = '%f'", x, y, width, height);
         fprintf(fp," fill = 'rgb(%d, %d, %d)' fill-opacity = '%f'", fill_r, fill_g, fill_b, fill_opacity);
