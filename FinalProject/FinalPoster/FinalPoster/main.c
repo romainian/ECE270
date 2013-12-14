07fc3f0e6a26853a4e66f528ec949319c5f54f38
Bradley Houghton <houghtonbradley@yahoo.com>
2013-12-14 12:26:55 -0500

    new code

--- FinalProject/FinalPoster/FinalPoster/main.c
+++ FinalProject/FinalPoster/FinalPoster/main.c
@@ -2,117 +2,76 @@
 #include <time.h>
 #include <math.h>
 #include <stdlib.h>
-#define MATRIX_HEIGHT 10
-#define MATRIX_WIDTH 10
+#define imWidth 1000
+#define imHeight 1000
+#define matWidth 10
+#define matHeight 10
 
-void Image(int matrix[MATRIX_HEIGHT][MATRIX_WIDTH])
+void printIntMatrix(int n, int m, int matrix[n][m])
 {
-    int i,j,flag;
-    flag=0;
-    for(i=0;i<MATRIX_HEIGHT;i++)
+    int i,j;
+    for(i=0;i<n;i++)
     {
-        for(j=0;j<MATRIX_WIDTH;j++)
+        for(j=0;j<m;j++)
         {
-            if(flag!=1)
-            {
-                if(matrix[i][j]==0)
-                {
-                    matrix[i][j]=1;
-                    flag=1;
-                }
-            }
+            printf("%d ", matrix[i][j]);
         }
-    }
-}
-void largeImage(int matrix[MATRIX_HEIGHT][MATRIX_WIDTH])
-{
-    int i,j,flag;
-    flag=0;
-    for(i=0;i<MATRIX_HEIGHT;i++)
-    {
-        for(j=0;j<MATRIX_WIDTH;j++)
-        {
-            if(flag!=1)
-            {
-                if(matrix[i][j]==0&&(MATRIX_WIDTH-j)>=2&&(MATRIX_HEIGHT-i)>=2)
-                {
-                    matrix[i][j]=2;
-                    matrix[i+1][j]=2;
-                    matrix[i][j+1]=2;
-                    matrix[i+1][j+1]=2;
-                    flag=1;
-                }
-            }
-        }
-    }
-}
-void largestImage(int matrix[MATRIX_HEIGHT][MATRIX_WIDTH])
-{
-    int i,j,flag;
-    flag=0;
-    for(i=0;i<MATRIX_HEIGHT;i++)
-    {
-        for(j=0;j<MATRIX_WIDTH;j++)
-        {
-            if(flag!=1)
-            {
-                if(matrix[i][j]==0&&(MATRIX_WIDTH-j)>=3&&(MATRIX_HEIGHT-i)>=3)
-                {
-                    matrix[i][j]=3;
-                    matrix[i+1][j]=3;
-                    matrix[i+2][j]=3;
-                    matrix[i][j+1]=3;
-                    matrix[i+1][j+1]=3;
-                    matrix[i+2][j+1]=3;
-                    matrix[i][j+2]=3;
-                    matrix[i+1][j+2]=3;
-                    matrix[i+2][j+2]=3;
-                    flag=1;
-                }
-            }
-        }
-    }
-}
-int main(int argc, const char * argv[])
-{
-    srand(time(NULL));
-    int i, j;
-    int N=MATRIX_HEIGHT;
-    int M=MATRIX_WIDTH;
-    int matrix[N][M];
-    //SETUP MATRIX
-    printf("\nMatrix Grid");
-    for(i=0;i<N;i++)
-    {
         printf("\n");
-        for(j=0;j<M;j++)
+    }
+}
+void setupZeroMatrix(int n, int m, int matrix[n][m])
+{
+    int i,j;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
         {
             matrix[i][j]=0;
-            printf("%d ",matrix[i][j]);
         }
     }
-    //SCAN FOR SPACE AND CHANGE
-    largestImage(matrix);       //
-    largeImage(matrix);         //
-    Image(matrix);              //  included in the main only two test the functions
-    largeImage(matrix);         //  it worked!
-    Image(matrix);              //
-    largeImage(matrix);         //
-    largestImage(matrix);       //
-    Image(matrix);              //
-    //PRINT NEW MATRIX
-    printf("\nMatrix Grid");
-    for(i=0;i<N;i++)
-    {
-        printf("\n");
-        for(j=0;j<M;j++)
-        {
-            printf("%d ",matrix[i][j]);
-        }
-    }
-    
-    
-    return 0;
 }
 
+int checkPosition(int i, int j, int n, int m, int matrix[n][m], int sizeCheck)
+{
+    int k, l;
+    int flag = 0;
+    for (k=i;k<i+sizeCheck;k++)
+    {
+        for (l=j;l<j+sizeCheck;l++)
+        {
+            if (matrix[k][l]!=0||(matWidth-l)<sizeCheck||(matHeight-k)<sizeCheck)
+            {
+                flag=1;
+            }
+        }
+    }
+    return flag;
+}
 
+void updatePosition(int i, int j, int n, int m, int matrix[n][m], int sizeCheck)
+{
+    int k, l;
+    for (k=i;k<i+sizeCheck;k++)
+    {
+        for (l=j;l<j+sizeCheck;l++)
+        {
+            matrix[k][l]=1;
+        }
+    }
+}
+
+void main()
+{
+    int test;
+    int placeMatrix[matHeight][matWidth];
+    setupZeroMatrix(matHeight, matWidth, placeMatrix);
+    placeMatrix[2][3]=0;
+    test =  checkPosition(1,2,matHeight,matWidth,placeMatrix, 3);
+    if(test==0)
+    {
+        updatePosition(1,2,matHeight, matWidth, placeMatrix, 3);
+    }
+    printIntMatrix(matHeight, matWidth, placeMatrix);
+    
+    
+}
