6b34e4d04b8966106c7cf3466fb28ebc9299448f
romainian <raptor53135@gmail.com>
2013-12-15 12:12:45 -0500

    a little bit of cleanup

--- FinalProject/FinalPoster/FinalPoster/main.c
+++ FinalProject/FinalPoster/FinalPoster/main.c
@@ -7,8 +7,13 @@
 #define matWidth 10
 #define matHeight 10
 #define classSize 100
+
 FILE *fp;
+char fileName[] = "poster.svg";
+
 float cellSize = imWidth/matWidth;
+
+
 // SVG HEADER AND FOOTER //
 void writeSVGHeader(char fileName[], int width, int height)
 {
@@ -23,8 +28,9 @@ void writeSVGFooter()
     fprintf(fp,"\n</svg>");
     fclose(fp);
 }
-// STRUCTURES //
 
+
+// STRUCTURES //
 struct Fill
 {
     int r;
@@ -40,6 +46,7 @@ struct Stroke
     int width;
     float opacity;
 };
+
 struct Circle
 {
     float x;
@@ -60,13 +67,38 @@ struct Rect
     float width;
     float height;
 };
+
 struct Student
 {
     int shape;   //0 if circle. 1 if rectangle
     int size;
 };
-// SET STRUCTURE FUNCTIONS //
 
+
+// SET STRUCTURE FUNCTIONS //
+void setFill(struct Fill *myFill,int r, int g, int b, float op)
+{
+    myFill->r=r;
+    myFill->g=g;
+    myFill->b=b;
+    myFill->opacity=op;
+}
+void setStroke(struct Stroke *myStroke, int red,int green,int blue,int width,float op)
+{
+    myStroke->r=red;
+    myStroke->g=green;
+    myStroke->b=blue;
+    myStroke->width=width;
+    myStroke->opacity=op;
+}
+
+void setCircle(struct Circle *myCircle,float cx,float cy, float r)
+{
+
+    myCircle->x=cx;
+    myCircle->y=cy;
+    myCircle->r=r;
+}
 void setLine(struct Line *myLine,float x1,float y1, float x2, float y2)
 {
     myLine->x1=x1;
@@ -74,12 +106,29 @@ void setLine(struct Line *myLine,float x1,float y1, float x2, float y2)
     myLine->x2=x2;
     myLine->y2=y2;
 }
-void setCircle(struct Circle *myCircle,float cx,float cy, float r)
+void setRect(struct Rect *myRect, int x, int y, int width, int height)
 {
+    myRect->x=x;
+    myRect->y=y;
+    myRect->width=width;
+    myRect->height=height;
+}
 
-    myCircle->x=cx;
-    myCircle->y=cy;
-    myCircle->r=r;
+void setFillArray(int Dim, struct Fill myFill[], int red[], int green[], int blue[], float opacity[])
+{
+    int i;
+    for(i=0;i<Dim;i++)
+    {
+        setFill(&myFill[i], red[i], green[i], blue[i], opacity[i]);
+    }
+}
+void setStrokeArray(int Dim, struct Stroke myStroke[], int red[], int green[], int blue[], int width[], float opacity[])
+{
+    int i;
+    for(i=0;i<Dim;i++)
+    {
+        setStroke(&myStroke[i], red[i], green[i], blue[i], width[i], opacity[i]);
+    }
 }
 void setCircleArray(int Dim, struct Circle myCircle[],float cx[],float cy[], float r[])
 {
@@ -90,45 +139,15 @@ void setCircleArray(int Dim, struct Circle myCircle[],float cx[],float cy[], flo
     }
 
 }
-void setRectangle(struct Rect *myRectangle, int x, int y, int width, int height)
+
+void setStudent(struct Student *myStudent, int shape_in, int size_in)
 {
-    myRectangle->x=x;
-    myRectangle->y=y;
-    myRectangle->width=width;
-    myRectangle->height=height;
+    myStudent->shape=shape_in;
+    myStudent->size=size_in;
 }
-void setFill(struct Fill *myFill,int r, int g, int b, float op)
-{
-    myFill->r=r;
-    myFill->g=g;
-    myFill->b=b;
-    myFill->opacity=op;
-}
-void setFillArray(int Dim, struct Fill myFill[], int red[], int green[], int blue[], float opacity[])
-{
-    int i;
-    for(i=0;i<Dim;i++)
-    {
-        setFill(&myFill[i], red[i], green[i], blue[i], opacity[i]);
-    }
-}
-void setStroke(struct Stroke *myStroke, int red,int green,int blue,int width,float op)
-{
-    myStroke->r=red;
-    myStroke->g=green;
-    myStroke->b=blue;
-    myStroke->width=width;
-    myStroke->opacity=op;
-}
-void setStrokeArray(int Dim, struct Stroke myStroke[], int red[], int green[], int blue[], int width[], float opacity[])
-{
-    int i;
-    for(i=0;i<Dim;i++)
-    {
-        setStroke(&myStroke[i], red[i], green[i], blue[i], width[i], opacity[i]);
-    }
-}
-// DRAW FUNCTIONS //
+
+
+// DRAWING FUNCTIONS //
 void drawCircle(float cx, float cy, float r,
                 int fill_r, int fill_g, int fill_b,
                 float fill_opacity, int stroke_r, int stroke_g,
@@ -155,18 +174,13 @@ void drawRectangle(float x, float y, float width, float height,
                    int fill_r, int fill_g, int fill_b, float fill_opacity,
                    int stroke_r, int stroke_g, int stroke_b, float stroke_opacity, int stroke_width)
 {
-    fprintf(fp,"\n <rect x = '%f' y = '%f' width = '%f' height = '%f'", x, y, width, height);
+    fprintf(fp,"\n    <rect x = '%f' y = '%f' width = '%f' height = '%f'", x, y, width, height);
     fprintf(fp," fill = 'rgb(%d, %d, %d)' fill-opacity = '%f'", fill_r, fill_g, fill_b, fill_opacity);
     fprintf(fp," stroke = 'rgb(%d, %d, %d)' stroke-opacity = '%f'", stroke_r, stroke_g, stroke_b, stroke_opacity);
     fprintf(fp," stroke-width = '%d' />", stroke_width);
 }
-// MATRIX //
 
-void setStudent(struct Student *myStudent, int shape_in, int size_in)
-{
-    myStudent->shape=shape_in;
-    myStudent->size=size_in;
-}
+// MATRIX AND POSITION //
 void printIntMatrix(int n, int m, int matrix[n][m])
 {
     int i,j;
@@ -207,7 +221,6 @@ int checkPosition(int i, int j, int n, int m, int matrix[n][m], int sizeCheck)
     }
     return flag;
 }
-
 void updatePosition(int i, int j, int n, int m, int matrix[n][m], int sizeCheck)
 {
     int k, l;
@@ -219,9 +232,9 @@ void updatePosition(int i, int j, int n, int m, int matrix[n][m], int sizeCheck)
         }
     }
 }
+
+
 // STUDENT FUNCTIONS //
-
-
 void drawImage(int i, int j, struct Student myStudent, int x)
 {
     float cx_in      = j*cellSize+myStudent.size*cellSize/2;
@@ -266,7 +279,7 @@ void main()
     int placeMatrix[matHeight][matWidth];
 
     x=0;
-    writeSVGHeader("Poster.SVG", 1000, 1000);
+    writeSVGHeader(fileName, imWidth, imHeight);
     setupZeroMatrix(matHeight, matWidth, placeMatrix);  //SVG
     setStudent(&myStudent[0], 0, 2);
     setStudent(&myStudent[1], 0, 2);
